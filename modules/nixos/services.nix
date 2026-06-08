7917dafbcea82955caa5e8e431e39623b002507c
pengeg <noreply@pengeg.com>
2026-06-08 22:32:20 +0000

    let me own the share

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -16,6 +16,9 @@ in
       ${mount} 192.168.1.0/24(rw,sync,no_subtree_check,root_squash,fsid=0)
     '';
   };
+  systemd.tmpfiles.rules = [
+    "d /backup 0755 pengeg users -"
+  ];
 
   # NFSv4 needs only 2049.
   networking.firewall.allowedTCPPorts = [ 2049 ];
