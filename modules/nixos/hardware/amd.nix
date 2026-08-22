6bc4cb6b1dab2cfae75498b0dee2374ee667ecac
gegnep <96670443+gegnep@users.noreply.github.com>
2026-08-22 05:45:19 +0000

    feat(amd): lact fan curve

--- modules/nixos/hardware/amd.nix
+++ modules/nixos/hardware/amd.nix
@@ -12,7 +12,41 @@ in
   options.mySystem.hardware.amd.enable = lib.mkEnableOption "amd stuffs";
 
   config = lib.mkIf cfg.enable {
-    hardware.amdgpu.initrd.enable = true;
+    hardware.amdgpu = {
+      initrd.enable = true;
+      overdrive = {
+        enable = true;
+        ppfeaturemask = "0xfffd7fff";
+      };
+    };
+
+    services.lact.enable = true;
+
+    environment.etc."lact/config.yaml".text = ''
+      version: 6
+      daemon:
+        log_level: info
+        admin_group: wheel
+      gpus:
+        "1002:7551-148C:2443-0000:13:00.0":
+          fan_control_enabled: true
+          fan_control_settings:
+            mode: curve
+            temperature_key: junction
+            interval_ms: 500
+            spindown_delay_ms: 5000
+            change_threshold: 2
+            curve:
+              45: 0.20
+              60: 0.24
+              72: 0.34
+              82: 0.55
+              92: 0.90
+          pmfw_options:
+            zero_rpm: false
+            minimum_pwm: 20
+            target_temperature: 80
+    '';
 
     # rocm
     hardware.graphics.enable = true;
